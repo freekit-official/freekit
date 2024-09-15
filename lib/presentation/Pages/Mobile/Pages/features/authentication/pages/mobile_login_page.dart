25b19efc8e0de14bd11182f04b68924d4b4ef94d
RISHIKESH-RICHI <rishikeshdeveloperrichi@gmail.com>
2024-09-15 03:27:39 -0700

    commit

--- lib/presentation/Pages/Mobile/Pages/features/authentication/pages/mobile_login_page.dart
+++ lib/presentation/Pages/Mobile/Pages/features/authentication/pages/mobile_login_page.dart
@@ -34,12 +34,56 @@ class MobileLoginPage extends StatelessWidget {
             hint: 'Password',
             isObscure: true,
           ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Forgot password?',
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: kBlue,
+                  fontFamily: 'Sora',
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
           const SignInButton(
             text: 'Login',
             background: kBlue,
             foreground: kWhite,
             padding: 7,
           ),
+          const SizedBox(
+            height: 12,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                'Doesn\'t have an account?',
+                style: TextStyle(
+                  fontFamily: 'Sora',
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              Text(
+                '  SignUp',
+                style: TextStyle(
+                  fontFamily: 'Sora',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
